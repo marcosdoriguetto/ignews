d5c3d267221a15dcb4ad8ab793063366030a169e
Marcos Fernandes <marcosdoriguetto@gmail.com>
2022-01-10 17:13:56 -0300

    Consuming Strapi API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
+
+type HomeProps = {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
  
-export default function Home() {
+export default function Home({ product }: HomeProps) {
   return (
     <>
     <Head>
@@ -12,12 +21,30 @@ export default function Home() {
       <section className={styles.hero}>
         <span>👏 Hey, welcome</span>
         <h1>News about the <span>React</span> world.</h1>
-        <p>Get access to all the publications <br/> <span>for $9.90 month</span></p>
-        <SubscribeButton />
+        <p>Get access to all the publications <br/> <span>for {product.amount} month</span></p>
+        <SubscribeButton priceId={product.priceId} />
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding" />
     </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KGTQCFdRh1V52ZBmJJ6NErU');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    }
+  };
 }
