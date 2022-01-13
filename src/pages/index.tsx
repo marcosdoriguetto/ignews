b01de36a84483f2278d062b7dce7824a72ccd164
Marcos Fernandes <marcosdoriguetto@gmail.com>
2022-01-13 15:10:23 -0300

    Add dynamic price and change SSR to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -10,28 +10,28 @@ type HomeProps = {
     amount: number;
   }
 }
- 
+
 export default function Home({ product }: HomeProps) {
   return (
     <>
-    <Head>
-      <title>Home | ig.news</title>
-    </Head>
-    <main className={styles.contentContainer}>
-      <section className={styles.hero}>
-        <span>👏 Hey, welcome</span>
-        <h1>News about the <span>React</span> world.</h1>
-        <p>Get access to all the publications <br/> <span>for {product.amount} month</span></p>
-        <SubscribeButton priceId={product.priceId} />
-      </section>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>Get access to all the publications <br /> <span>for {product.amount} month</span></p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
 
-      <img src="/images/avatar.svg" alt="Girl coding" />
-    </main>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KGTQCFdRh1V52ZBmJJ6NErU');
 
   const product = {
@@ -42,9 +42,12 @@ export const getServerSideProps: GetServerSideProps = async () => {
     }).format(price.unit_amount / 100),
   };
 
+  const revalidateTime = 60 * 60 * 24; // 24 hours
+
   return {
     props: {
       product,
-    }
+    },
+    revalidate: revalidateTime,
   };
 }
