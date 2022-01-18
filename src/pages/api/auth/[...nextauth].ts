dac66a30ba6bad773d5e2316051cf1b83c21816f
Marcos Fernandes <marcosdoriguetto@gmail.com>
2022-01-18 11:09:59 -0300

    Insert user autentication in DB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -8,4 +11,40 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, profile, account }) {    
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('users_by_email'),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              { data: { email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('users_by_email'),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch (error) {
+        console.log(error);
+        return false
+      }
+
+    },
+  }
 })
